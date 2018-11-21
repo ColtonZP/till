925b1a98fb0012e3ffeb4f998776e58bf7c94aff
Colton Pemberton <czpemberton@gmail.com>
2018-11-20 20:05:59 -0800

    Update

--- calculate.js
+++ calculate.js
@@ -1,26 +1,29 @@
-function cal(){
-    document.getElementById("100dt").innerHTML = ("$" + document.getElementById("100da").value * 100);      var tot = document.getElementById("100da").value * 100;
-    document.getElementById("50dt").innerHTML = ("$" + document.getElementById("50da").value * 50);         tot = tot + (document.getElementById("50da").value * 50);
-    document.getElementById("20dt").innerHTML = ("$" + document.getElementById("20da").value * 20);         tot = tot + (document.getElementById("20da").value * 20);
-    document.getElementById("10dt").innerHTML = ("$" + document.getElementById("10da").value * 10);         tot = tot + (document.getElementById("10da").value * 10);
-    document.getElementById("5dt").innerHTML = ("$" + document.getElementById("5da").value * 5);            tot = tot + (document.getElementById("5da").value * 5);
-    document.getElementById("1dt").innerHTML = ("$" + document.getElementById("1da").value * 1);            tot = tot + (document.getElementById("1da").value * 1);
-    document.getElementById("25ct").innerHTML = ("$" + (document.getElementById("25ca").value * 25)/100);   var ctot = (document.getElementById("25ca").value * 25);
-    document.getElementById("10ct").innerHTML = ("$" + (document.getElementById("10ca").value * 10)/100);   ctot = ctot + (document.getElementById("10ca").value * 10);
-    document.getElementById("5ct").innerHTML = ("$" + (document.getElementById("5ca").value * 5)/100);      ctot = ctot + (document.getElementById("5ca").value * 5);
-    document.getElementById("1ct").innerHTML = ("$" + (document.getElementById("1ca").value * 1)/100);      ctot = ctot + (document.getElementById("1ca").value * 1);
-    document.getElementById("25rt").innerHTML = ("$" + document.getElementById("25ra").value * 10);         tot = tot + (document.getElementById("25ra").value * 10);
-    document.getElementById("10rt").innerHTML = ("$" + document.getElementById("10ra").value * 5);          tot = tot + (document.getElementById("10ra").value * 5);
-    document.getElementById("5rt").innerHTML = ("$" + document.getElementById("5ra").value * 2);            tot = tot + (document.getElementById("5ra").value * 2);
-    document.getElementById("1rt").innerHTML = ("$" + (document.getElementById("1ra").value * 50)/100);     ctot = ctot + (document.getElementById("1ra").value * 50);
-    var ftot = tot + (ctot/100);
-    document.getElementById("totaled").innerHTML = ("$" + ftot);
+var denArray = [100, 50, 20, 10, 5, 1, .25, .10, .05, .01, 10, 5, 2, .5];
+var sum;
+
+const table = document.querySelector('table');
+const input = document.querySelectorAll('input');
+const total = document.querySelectorAll('#total');
+const totalSum = document.querySelector('#totalSum');
+
+table.addEventListener('input', (event) => {
+    for (let i = 0; i < input.length; i++) {
+        if (event.target === input[i]) { 
+            total.textContent = '0';
+            total[i].textContent = '$' + calc(event.target.value, denArray[i]);
+        }
+    }
+    totalSum.textContent = addAll();
+});
+
+function calc(inp, den) {
+    return ((inp * 100) * den)/100;
 }
 
-function isNumberKey(evt) {
-        var charCode = (evt.which) ? evt.which : event.keyCode
-        if (charCode > 31 && (charCode < 48 || charCode > 57))
-            return false;
-
-        return true;
+function addAll() {
+    sum = 0;
+    for (let i = 0; i < total.length; i++) {
+        sum += Number(total[i].textContent.substr(1, total[i].textContent.length))*100;
+    }
+    return sum/100;
 }
